58f31b3f8dedcd6135c5688b3676b63861c99a40
Agustin Fernandez <agustinf12321@gmail.com>
2024-11-13 18:09:47 -0300

    modelo tabla medicos

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'cdoctor_name',
+        'ndoctor_dni',
+        'cdoctor_address',
+        'id_speciality',
+        'ndoctor_tuition', //matricula
+        'cdoctor_phone',
+        'ddoctor_startdate',
+    ];
+
+    public function Speciality()
+    {
+        return $this->belongsTo(Speciality::class, 'id_speciality');
+    }
 }
--- database/migrations/2024_11_11_205901_create_doctors_table.php
+++ database/migrations/2024_11_11_205901_create_doctors_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('cdoctor_name'); // Nombre del médico
+            $table->integer('ndoctor_dni'); // DNI del médico
+            $table->string('cdoctor_address'); // Dirección del médico
+            $table->foreignId('id_speciality')->constrained('specialities')->onDelete('cascade'); // Relación con la tabla especialidades
+            $table->integer('ndoctor_tuition'); // Matrícula del médico
+            $table->string('cdoctor_phone'); // Teléfono del médico
+            $table->date('ddoctor_startdate')->nullable(); // Fecha de inicio
         });
     }
 
